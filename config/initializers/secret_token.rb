8a8c14e770b4d58d441982789122177f67be1c6a
junky <junky@Junky.Home>
2012-03-05 22:31:03 +0200

    Init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Elections::Application.config.secret_token = '20987a7b61c04bc6f64eff67fdd49b94bf342607e8770d8a620d70b58c8d33e7112ac32dcf5f90993016415de2f179222afde0796f64ee2b143dc7d48ea015a2'
