0273d66afe2da9a8f7acaafdfd72004860d4572e
junky <junky@Junky.Home>
2012-03-26 00:11:23 +0300

    Updated pages controller.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,10 +1,13 @@
 class PagesController < ApplicationController
   def home
+    @home_class = 'class="active"'
   end
 
   def contact
+    @contact_class = 'class="active"'
   end
 
   def election_distract
+    @distr_class = 'class="active"'
   end
 end
