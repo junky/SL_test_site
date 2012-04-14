2c7d7015b75b53236529d4baaf72e471ea49c88d
junky <junky@Junky.Home>
2012-04-14 23:51:30 +0300

    Implemented contact form

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,3 +1,5 @@
+# encoding: utf-8
+
 class PagesController < ApplicationController
   def home
     @home_class = 'class="active"'
@@ -5,6 +7,14 @@ class PagesController < ApplicationController
 
   def contact
     @contact_class = 'class="active"'
+    
+    name = params[:name]
+    email = params[:email]
+    text = params[:text]
+    if not name.nil? and not email.nil? and not text.nil? \
+      and not name.empty? and not email.empty? and not text.empty?
+      @contact = Contact.create(:name => name, :email => email, :text => text)
+    end
   end
 
   def election_distract
