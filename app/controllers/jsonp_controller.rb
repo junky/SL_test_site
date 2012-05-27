c48fe43a2f4a8b8c5068b47ee1a84e9d0e32c301
junky <junky@Junky.Home>
2012-05-27 19:22:09 +0300

    Removed /n symbols from json content.

--- app/controllers/jsonp_controller.rb
+++ app/controllers/jsonp_controller.rb
@@ -1,4 +1,5 @@
 # encoding: utf-8
+require 'net/http'
 
 class JsonpController < ApplicationController
   def jsonp
@@ -7,6 +8,7 @@ class JsonpController < ApplicationController
       if not url.nil? and not function_name.nil?
         json_str = Net::HTTP.get(URI(url))
         json_str.gsub!('"', '\"')
+        json_str.gsub!("\n", '')
         render :inline => function_name + '("'+ json_str + '")'
       else
         render :inline => ''
