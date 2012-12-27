eea70e8f1d0d6a9a5bcd613484b97dff2693ce34
junky <junky@gala.net>
2012-12-27 12:45:48 +0200

    Created Rails app.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SLTestSite::Application.config.secret_token = 'b897534ae20a8659f17a34329b8f3651b6df7b80bc8c57e0dad6746187fc7f14ec57f9259162ce8b395c8c4f728f316582293c8e8f2ff9d4d2168821197db268'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+SLTestSite::Application.config.session_store :cookie_store, key: '_SL_test_site_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# SLTestSite::Application.config.session_store :active_record_store
